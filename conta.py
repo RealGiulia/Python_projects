2e68ba1f5397ef998a0fa3ed4b1aab3f91d5eed5
RealGiulia <84285314+RealGiulia@users.noreply.github.com>
2021-06-22 10:49:25 -0300

    Add files via upload
    Introduzindo os estudos sobre POO, criei a classe conta. Nela forma atribuídas alguns valores e alguns métodos de responsabilidade da classe e utilizando o parâmetro "Self". Ainda é um projeto inicial, tendo muito o que melhorar. Logo postarei novamente esse projeto, porém ele estará mais completo e sofisticado. Vejo o projeto conta.py como um bom guia para iniciar os estudos sobre POO, pois apresenta todos os conceitos do paradigma de forma bem simples e didática.

---
+++ conta.py
@@ -0,0 +1,22 @@
+
+
+class Cria_conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto...{}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+
+    def extrato(self):
+        print("O saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def depositar(self, valor):
+        self.__saldo += valor
+
+    def sacar(self, valor):
+        self.__saldo -= valor
+
+    def transferir(self, valor, destino):
+        self.sacar(valor)
+        destino.depositar(valor)
