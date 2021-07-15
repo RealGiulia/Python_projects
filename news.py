3a8cda13f7836e323133a6dfdbb6f1f17b63eb10
RealGiulia <84285314+RealGiulia@users.noreply.github.com>
2021-07-15 12:16:59 -0300

    Add files via upload

--- news.py
+++ news.py
@@ -1,16 +1,18 @@
 from gnews import GNews
 
-google_news = GNews()
-google_news.language = 'pt-br'
-google_news.max_results = 10
-google_news.country = 'Brazil'
-google_news.period = '3d'
-busca = input("Digite o que você quer buscar: ")
-result = google_news.get_news(busca)
 
-for x in result:
-    print("----------------------||-------------------------------" * 2)
-    print("Titulo: ", x['title'])
-    print("Data ", x['published date'])
-    print("Descrição---", x['description']),
-    print("Link--- ", x['url'])
+class Noticia:
+    def __init__(self):
+        google_news = GNews()
+        google_news.language = 'pt-br'
+        google_news.max_results = 10
+        google_news.country = 'Brazil'
+        google_news.period = '3d'
+        busca = 'mercado de ações'
+        result = google_news.get_news(busca)
+
+        for x in result:
+            print("Titulo: ", x['title'])
+            print("Data ", x['published date'])
+            print("Descrição---", x['description']),
+            print("Link--- ", x['url'])
